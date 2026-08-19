faef9581fd5ec1c76e7babcd157c33807fc2fc0a
ronald <ronald34n@gmail.com>
2026-08-18 23:37:10 -0400

    corrigiendo test

--- src/test/java/com/cicd/webapi/SimpleTest.java
+++ src/test/java/com/cicd/webapi/SimpleTest.java
@@ -1,14 +1,13 @@
 package com.cicd.webapi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 public class SimpleTest {
-    
+
     @Test
     void testHolaMundo() {
         String mensaje = "Hola Mundo CI/CD";
-        assertEquals("Hola Mundo CI/CD", mensaje);
+        Assertions.assertEquals("Hola Mundo CI/CD", mensaje);
     }
 }
